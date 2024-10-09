467dffa1736c0223bf095cd74881f276095d7c2f
Joo Manasseh <maphadathrowmhijoo@gmail.com>
2024-10-09 23:54:43 +0000

    Featured Box added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,20 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
+    minHeight: {
+      '0': '0',
+      '1/8': '80px',
+      '7/10': '700px',
+    },
     extend: {
       fontFamily: {
         body: 'Poppins'
       },
       colors: {
         'body-color': '#fafafa',
-        'btn-color': '#efefef'
+        'btn-color': '#efefef',
+        'feat-box-color': '#c0a631',
+        'feat-text-col': '#444444',
       },
       lineHeight: {
         'extra-loose': '5.5'
